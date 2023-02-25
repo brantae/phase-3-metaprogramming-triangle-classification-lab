d664756fc13025a667a9ea5a70aa98614ef5daec
Anslie Brant <brantae@gmail.com>
2023-02-25 17:35:05 -0600

    commit tree 8e930f6ba89733cbd73dba43a5912f273fc7f963

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if (side_a > 0 && side_b > 0 && side_c > 0) && (side_a + side_b > side_c && side_b + side_c > side_a && side_a + side_c > side_b)
+      if side_a == side_b && side_b == side_c
+      :equilateral
+      elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+      else
+      :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+
+
+  class TriangleError < StandardError
+  end
+
 end
